aa3e8721e2964ade20207480b330a3b156c3cb0f
chiao115 <yyqxwqx@gmail.com>
2025-05-12 20:39:38 +0800

    5/12 last week done

--- src/app/croissant/page.js
+++ src/app/croissant/page.js
@@ -4,20 +4,14 @@ import QuestionPage from '@/component/page/QuestionPage';
 import DisplayResultPage from '@/component/page/DisplayresultPage';
 import ResultPage from '@/component/page/ResultPage';
 import { useState } from 'react';
-import {usePsyStore, usePsyStore} from '@/app/store/store'
+import {usePsyStore} from '@/app/store/store'
 
 export default function Croissant() {
 
-  const usePsyStore = usePsyStore((state) => state);
+  const psyState = usePsyStore( (state) => state );
 
-  const [psyState, setGameState] = useState({
-    state: 0, //0:start, 1:question, 2:displayResult, 3: result
-    questionState: 0,
-    totalQuestions: 3,
-    score: 0
-  });
-  
   const nextStep = function(){
+    
     if(psyState.state >= 3) return;
 
     if(psyState.state == 1){
@@ -25,22 +19,14 @@ export default function Croissant() {
       //要超過總題數才能結束答題階段
 
       if(psyState.questionState < psyState.totalQuestions-1){
-
-        psyState.updatequestionState(psyState.questionState + 1)
- 
+        psyState.updateQuestionState(psyState.questionState + 1);
       }else{
-        setGameState({
-          ...psyState,
-          state: psyState.state + 1
-        });
+        psyState.updateState(psyState.state + 1);
       }
       
     }else{
       console.log("next");
-      setGameState({
-        ...psyState,
-        state: psyState.state + 1
-      });
+      psyState.updateState(psyState.state + 1);
     }
 
   }
@@ -48,28 +34,21 @@ export default function Croissant() {
   const prevStep = function(){
     if(psyState.state <= 0) return;
     console.log("prev");
-    setGameState({
-      ...psyState,
-      state: psyState.state - 1
-    })
+    psyState.updateState(psyState.state - 1);
   }
-
 
 
   return (
     <>
-      <div className='w-screen h-screen bg-amber-50 flex justify-center items-center'>
-        { psyState.state ==0 && <StartPage nextStep={nextStep}/>}
-        { psyState.state ==1 && <QuestionPage questionIndex={psyState.questionstate}/>}
-        { psyState.state ==2 && <DisplayResultPage/>}
-        { psyState.state ==3 && <ResultPage/>}
-
-        {/* <div onClick={prevStep}>上一步</div> */}
-
-        {/* <div onClick={nextStep}>下一步</div> */}
+      <div className="w-screen h-screen bg-gray-200 flex justify-center items-center">
+        { psyState.state == 0 && <StartPage nextStep={nextStep} />}
+        { psyState.state == 1 && <QuestionPage nextStep={nextStep} questionIndex={psyState.questionState} />}
+        { psyState.state == 2 && <DisplayResultPage/>}
+        { psyState.state == 3 && <ResultPage/>}
         
+        {/* <div onClick={prevStep}>上一步</div>
+        <div onClick={nextStep}>下一步</div> */}
       </div>
-      
     </>
   );
 }
--- src/component/layout/MobileFrame.js
+++ src/component/layout/MobileFrame.js
@@ -5,7 +5,9 @@ export default function DisplayresultPage({children}) {
 
   return (
     <>
-      <div className='w-[33%] min-w-[380px] max-w-[420px] h-[85%] bg-white rounded-2xl flex justify-center items-center relative overflow-hidden'>
+      <div className='w-[33%] min-w-[380px] max-w-[420px] h-[85%] p-[52px] bg-white 
+        rounded-2xl flex justify-center items-center
+         relative overflow-hidden'>
           {children}
       </div>
     </>
